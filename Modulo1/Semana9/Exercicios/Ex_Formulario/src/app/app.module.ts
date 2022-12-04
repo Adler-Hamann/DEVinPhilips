09f25385f71e102270de53edc04b6aeeee8cbe62
Adler-Hamann <adlerhamannmartins@gmail.com>
2022-12-04 20:52:14 -0300

    Exercicio 2 da semana 9

--- Modulo1/Semana9/Exercicios/Ex_Formulario/src/app/app.module.ts
+++ Modulo1/Semana9/Exercicios/Ex_Formulario/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
