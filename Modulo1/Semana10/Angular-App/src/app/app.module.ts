8a43eb801da4a95583ef908e40bcfd4851ede806
Adler-Hamann <adlerhamannmartins@gmail.com>
2023-01-14 17:53:11 -0300

    Adicionado Exercicio 2 da Semana 10

--- Modulo1/Semana10/Angular-App/src/app/app.module.ts
+++ Modulo1/Semana10/Angular-App/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FiltroComponent } from './filtro/filtro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationListComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FiltroComponent
   ],
   imports: [
     BrowserModule
